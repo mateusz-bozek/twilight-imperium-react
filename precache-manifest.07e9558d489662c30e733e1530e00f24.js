907eef8df7dab775153a643b6bb251a8298cf539
Mateusz <mateusz@Mateusz-2.local>
2020-10-04 22:16:15 +0200

    Updates

---
+++ precache-manifest.07e9558d489662c30e733e1530e00f24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a277a65b56b47029779b38acc48d7877",
+    "url": "/twilight-imperium-react/index.html"
+  },
+  {
+    "revision": "a03863927c75ffcb88a4",
+    "url": "/twilight-imperium-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d44e9ec40b27cc8979e8",
+    "url": "/twilight-imperium-react/static/js/2.71b846f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/twilight-imperium-react/static/js/2.71b846f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a03863927c75ffcb88a4",
+    "url": "/twilight-imperium-react/static/js/main.116680aa.chunk.js"
+  },
+  {
+    "revision": "5055e51cd6adb4a2eafe",
+    "url": "/twilight-imperium-react/static/js/runtime-main.178a346e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/twilight-imperium-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
